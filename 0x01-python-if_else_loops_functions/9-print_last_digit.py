c7286057284f8175e1911e2c4e795172a73650a5
Otavie <otavieokuoyo.gmail.com>
2023-08-08 16:09:44 -0700

    Modified 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    number = abs(number % 10);
-    print(number, end="")
-    return number
+    last_digit = number % 10;
+    print(last_digit, end="")
+    return last_digit
