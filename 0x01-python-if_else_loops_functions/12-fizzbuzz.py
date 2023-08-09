51a5361ec0ce84d254a73e947932a6b68617160e
Otavie <otavieokuoyo.gmail.com>
2023-08-09 02:35:15 -0700

    Add the code for 120fizzbuzz.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0:
+            print("{}".format("Fizz"), end=" ")
+        elif i % 5 == 0:
+            print("{}".format("Buzz"), end=" ")
+        elif i % 15 == 0:
+            print("{}".format("FizzBuzz"), end=" ")
+        else:
+            print("{}".format(i), end=" ")
+
