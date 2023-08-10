0e597a3421c5f43384ababa4908db147ead27383
Otavie <otavieokuoyo.gmail.com>
2023-08-10 13:32:30 -0700

    Modified the code in 2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-from sys import argv
 
-if len(argv) - 1 == 0:
-    print("{} argument:".format(len(argv) - 1))
-elif len(argv) - 1 == 1:
-    print("{} argument:".format(len(argv) - 1))
-elif len(argv) - 1 > 1:
-    print("{} arguments:".format(len(argv) - 1))
+if __name__ == "__main__":
+    from sys import argv
 
-for i in range(len(argv) - 1):
-    print("{}: {}".format((i + 1), argv[i + 1]))
+    if len(argv) - 1 == 0:
+        print("{} argument:".format(len(argv) - 1))
+    elif len(argv) - 1 == 1:
+        print("{} argument:".format(len(argv) - 1))
+    elif len(argv) - 1 > 1:
+        print("{} arguments:".format(len(argv) - 1))
+
+    for i in range(len(argv) - 1):
+        print("{}: {}".format((i + 1), argv[i + 1]))
