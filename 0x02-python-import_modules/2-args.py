a42027aeec21d3543944358f191a7b7548916409
Otavie <otavieokuoyo.gmail.com>
2023-08-10 13:54:35 -0700

    Modifie dthe code for 2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     from sys import argv
 
     if len(argv) - 1 == 0:
-        print("{} argument.".format(len(argv) - 1))
+        print("{} arguments.".format(len(argv) - 1))
     elif len(argv) - 1 == 1:
         print("{} argument:".format(len(argv) - 1))
     elif len(argv) - 1 > 1:
