2786749feebd4dd4c95aac4ea284c58d222ff20a
Otavie <otavieokuoyo.gmail.com>
2023-08-10 13:34:29 -0700

    Modified the code in 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-from sys import argv
 
-sum = 0
+if __name__ == "__main__":
+    from sys import argv
 
-for i in range(len(argv) - 1):
-    sum = sum + int(argv[1 + i])
-print(sum)
+    sum = 0
+
+    for i in range(len(argv) - 1):
+        sum = sum + int(argv[1 + i])
+    print(sum)
