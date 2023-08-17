b37f1f42910fee1d1f5e5da7511355a000ae2751
Otavie <otavieokuoyo.gmail.com>
2023-08-16 17:23:04 -0700

    Modified the code for 10-best_score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,5 +3,5 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
 
-    max_value = max(a_dictionary.values())
-    return max_value
+    max_key = max(a_dictionary, key=a_dictionary.get)
+    return max_key
