0f355ae361b1688eb80c6d8b51ad02a590c88238
Otavie <otavieokuoyo.gmail.com>
2023-08-29 05:09:35 -0700

    Modified the code in 0-square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-"""Defines a class called Square"""
-
 class Square:
-    """Class for the attributes amd properties of square
-    """
+    """Class for the attributes amd properties of square"""
     pass
