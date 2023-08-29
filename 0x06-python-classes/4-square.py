70b7a267c52e5ed2d36421901b9e5ae75def1e82
Otavie <otavieokuoyo.gmail.com>
2023-08-29 05:39:53 -0700

    Modified the code for 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,14 +1,40 @@
 #!/usr/bin/python3
 class Square:
+    """
+    Class that defines the properties of square based on 3-square.py
+
+    Attributes:
+        size: size of a side of the square
+    """
+
     def __init__(self, size=0):
+
+        """Create a class constructor
+        Args:
+            size: size of a side of the square
+        """
+
         self.__size = size
 
     @property
     def size(self):
+        """Returns the size of the square"""
+
         return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        Property Setter for size
+
+        Args:
+            value: size of a side of the square
+
+        Raises:
+            TypeError: size must be an integer
+            ValueError: size must be >= 0
+        """
+
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -16,4 +42,7 @@ class Square:
         self.__size = value
 
     def area(self):
+
+        """Calculates and returns the area of the square"""
+
         return self.__size * self.__size
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 class Square:
+    """
+    Class that defines properties of squred based on 
     def __init__(self, size=0):
         self.__size = size
 
