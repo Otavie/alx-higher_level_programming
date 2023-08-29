82a8b7c5cf12f1036529c73434ebbe346192f3fd
Otavie <otavieokuoyo.gmail.com>
2023-08-29 05:17:39 -0700

    Modified the code in 1-square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+
+"""An empty class Square that defines a square"""
+
 class Square:
+    """
+    Class for the attributes amd properties of square
+    Attributes:
+        size: length of a side of a square
+    """
     def __init__(self, size):
+        
+        """Class constructor 
+        Args:
+            size: length of a side of a square
+        """
+
         self.__size = size
