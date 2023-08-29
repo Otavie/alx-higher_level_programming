2808842d348f8028746ddb7db9a91864dcf67218
Otavie <otavieokuoyo.gmail.com>
2023-08-29 05:23:31 -0700

    Modified the code in 2-square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python3
+
+"""An empty class Square that defines a square"""
+
 class Square:
+
+    """
+    Class for the attributes amd properties of square
+    Attributes:
+        size: length of a side of a square
+    """
+
     def __init__(self, size=0):
+
+        """Class constructor
+        Args:
+            size: length of a side of a square
+        """
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
