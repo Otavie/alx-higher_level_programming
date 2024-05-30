162d8ff2855819f08e7dce8d1739fba9a5f66224
Otavie <otavieokuoyo@gmail.com>
2024-05-30 11:52:28 -0700

    replcd args with args[0] in condn stmt

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// Get the arguments passed to the script
+const args = process.argv.slice(2);
+
+if (args[0] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(args[0]);
+}
