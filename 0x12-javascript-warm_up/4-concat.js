1a54aff0fff5f15f59aba5818bcc3940e6810ceb
Otavie <otavieokuoyo@gmail.com>
2024-05-30 12:01:59 -0700

    Add the code for 4-concat.js

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+// Get the arguments passed to the script
+const args = process.argv.slice(2);
+
+console.log(args[0] + ' is ' + args[1]); 
+/*
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
+*/
