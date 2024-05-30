0017468980592684ba3b9274dbbc5f14d3f1971b
Otavie <otavieokuoyo@gmail.com>
2024-05-30 16:16:57 -0700

    Add the codes for 9-add

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const args1 = parseInt(args[0], 10);
+const args2 = parseInt(args[1], 10);
+
+function add (a, b) {
+  console.log(a + b);
+}
+
+if (isNaN(args1)|| args.length !== 2) {
+  console.log('NaN');
+} else {
+  add(args1, args2);
+}
