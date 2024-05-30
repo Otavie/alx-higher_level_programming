2e5a1fd72ce69e0d99049f03c4ec9409f2b324b7
Otavie <otavieokuoyo@gmail.com>
2024-05-30 12:27:10 -0700

    Add the codes for 5-to_integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// Get the arguments passed to the script
+const args = process.argv.slice(2);
+
+let number = parseInt(args[0], 10);
+
+if (isNaN(number)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + number);
+}
