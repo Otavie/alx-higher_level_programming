a5ccf0c6517ffb2c98a8464f7fd758cc1b2e6b73
Otavie <otavieokuoyo@gmail.com>
2024-05-30 12:48:02 -0700

    Changed arr from let to const

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 let i = 0;
-let arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 while (i < 3) {
   console.log(arr[i]);
   i++;
