d125109cba89c174932e453eb90d14e89cc61413
Otavie <otavieokuoyo@gmail.com>
2024-05-31 04:28:42 -0700

    Modified the code again

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 const arrs = process.argv.slice(2);
-const arrNumbers = arrs.sort();
+const arrNumbers = arrs.sort().reverse();
 
-if (arrs.length <= 2) {
+if (arrNumbers.length <= 2) {
   console.log(0);
 } else {
-  console.log(arrNumbers[1]);
+  console.log(arrNumbers);
 }
