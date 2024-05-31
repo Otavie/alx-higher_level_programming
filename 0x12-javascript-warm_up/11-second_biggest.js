84980765c641e485b0c474b1c0f4bc5389d8e320
Otavie <otavieokuoyo@gmail.com>
2024-05-31 04:41:40 -0700

    Removed revserse()

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 const arrs = process.argv.slice(2);
-const arrNumbers = arrs.sort().reverse();
+const arrNumbers = arrs.sort();
 
 if (arrNumbers.length <= 1) {
   console.log(0);
 } else {
-  console.log(arrNumbers[1]);
+  console.log(arrNumbers[arrNumbers.length - 2]);
 }
