1ed33704e7f88e0a2a3c36025c7175cb061cdc47
Otavie <otavieokuoyo@gmail.com>
2024-05-31 04:36:09 -0700

    Modified the condnal to <=1

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 const arrs = process.argv.slice(2);
 const arrNumbers = arrs.sort().reverse();
 
-if (arrNumbers.length <= 2) {
+if (arrNumbers.length <= 1) {
   console.log(0);
 } else {
   console.log(arrNumbers[1]);
