2d33f3eae57b8d5943ac658118c4559636445cb3
Otavie <otavieokuoyo@gmail.com>
2024-05-31 03:57:43 -0700

    Sort numers in descending order

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,16 +3,12 @@
 let arrs = process.argv.slice(2);
 let arrNumbers = arrs.map(Number);
 
-function sortNumbers(arr) {
-  return arr.sort((a, b) => a - b);
-}
-
-let sortedNumbers = sortNumbers(arrNumbers);
+arrNumbers.sort((a, b) => b - a);
 
 if (arrs.length === 0) {
   console.log(0);
 } else if (arrs.length === 1) {
   console.log(1);
 } else {
-  console.log(sortedNumbers[sortedNumbers.length - 2]);
+  console.log(arrNumbers[1]);
 }
