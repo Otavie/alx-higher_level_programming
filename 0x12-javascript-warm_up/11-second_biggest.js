68d0898193df6a2ec1d577c2e62216fc7f7c4423
Otavie <otavieokuoyo@gmail.com>
2024-05-31 04:08:10 -0700

    Chanhged the condn to meet the criteria

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,10 +5,8 @@ const arrNumbers = arrs.map(Number);
 
 arrNumbers.sort((a, b) => b - a);
 
-if (arrs.length === 0) {
+if (arrs.length <= 2) {
   console.log(0);
-} else if (arrs.length === 1) {
-  console.log(1);
 } else {
   console.log(arrNumbers[1]);
 }
