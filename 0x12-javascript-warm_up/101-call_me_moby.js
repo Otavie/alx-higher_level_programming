71e80226792921ba4bc16cdc3a2cfb9cc5e70b4c
Otavie <otavieokuoyo@gmail.com>
2024-05-30 17:23:01 -0700

    Change export from module export to named export

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
-function callMeMoby (x, theFunction) {
+exports.callMeMoby = function (x, theFunction) {
  for (let i = 0; i < x; i++) {
    theFunction();
  }
 }
-
-module.exports = { callMeMoby };
