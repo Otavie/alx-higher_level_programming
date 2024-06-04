0e1a58736e4285474d2cb35c4a178da22c34dc9c
Otavie <otavieokuoyo@gmail.com>
2024-06-04 08:52:06 -0700

    Add the code for 9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if (len(my_list) == 0):
+        return None
+    
+    max_num = my_list[0]
+    for i in my_list:
+        if i > max_num:
+            max_num = i
+
+    return (max_num)
+
