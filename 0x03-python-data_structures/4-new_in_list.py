8020b0ab7c1417b0fb5a2b1fbb95053084d5a1b4
Otavie <otavieokuoyo@gmail.com>
2024-06-03 20:48:36 -0700

    Modified out of range condition

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     # Check if idx is -ve r out of range
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx > len(my_list) - 1:
         return ("{}".format(my_list[:]))
 
     # Create a New List
