7fcb644d344dec80476d9b74fc30918956490e58
Otavie <otavieokuoyo@gmail.com>
2024-06-04 04:44:09 -0700

    Remove print format

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    # Check if idx is -ve r out of range
     if idx < 0 or idx >= len(my_list):
-        return ("{}".format(my_list[:]))
+        return (my_list[:])
 
     # Create a New List
     new_list = my_list[:]
     my_list[idx] = element
-    return ("{}".format(my_list))
+    return (my_list)
