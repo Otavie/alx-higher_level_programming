f8498d26fb851f1d386d863947454ed61f7aa1b1
Otavie <otavieokuoyo@gmail.com>
2024-06-03 20:46:00 -0700

    Create a new list after if stmt

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_list = my_list[:]
     # Check if idx is -ve r out of range
     if idx < 0 or idx > len(my_list):
         return ("{}".format(my_list[:]))
 
+    # Create a New List
+    new_list = my_list[:]
     my_list[idx] = element
     return ("{}".format(my_list))
