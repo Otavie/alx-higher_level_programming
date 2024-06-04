726158c864ad77fa8f944967e922d6a315c8904a
Otavie <otavieokuoyo@gmail.com>
2024-06-04 05:53:13 -0700

    Add the code for 6-ptrint_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i, num in enumerate(row):
+            if i != 0:
+                print(" ", end="")
+            print()
