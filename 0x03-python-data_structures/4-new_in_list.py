46c69e98d89911b20c57d8b84ec3a15faa607546
Otavie <otavieokuoyo@gmail.com>
2024-06-03 20:42:44 -0700

    Adhere to reqmt of returning the original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new_list = my_list[:]
+    # Check if idx is -ve r out of range
     if idx < 0 or idx > len(my_list):
-        return ("{}".format(new_list))
+        return ("{}".format(my_list[:]))
+
     my_list[idx] = element
     return ("{}".format(my_list))
