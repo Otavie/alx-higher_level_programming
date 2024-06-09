18f7d659435b87b3bdfde3fef9fbdecf1c49656d
Otavie <otavieokuoyo@gmail.com>
2024-06-09 13:42:21 -0700

    Add limit to the script

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,4 +1,4 @@
 #!/usr/bin/env mysql
 -- Script to display the max temperature of each state ordered by state name
 
-SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state DESC;
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state ASC LIMIT 3;
