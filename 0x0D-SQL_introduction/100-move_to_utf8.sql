97cc58ab0420f20eeabe84a3bd12a636ca79ec22
Otavie <otavieokuoyo@gmail.com>
2024-06-09 13:13:06 -0700

    Add the script for 100-move_to_utf8.sql

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,11 @@
+#!/usr/bin/env mysql
+-- Script to convert the database, table, and column to UTF8
+
+-- Convert the database
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+-- Convert the table
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the column
+ALTER TABLE first_second MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
