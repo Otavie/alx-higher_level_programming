3aa29fbbdb1a28bd8c84430837480f7c02f67aee
Otavie <otavieokuoyo@gmail.com>
2024-06-09 10:27:00 -0700

    Removed the word SELECT

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 #!/usr/bin/env mysql
 -- A script that prints the full description of the table first_table from the database hbtn_0c_0 in the MySQL server
 
-SELECT SHOW CREATE first_table;
+SHOW CREATE first_table;
