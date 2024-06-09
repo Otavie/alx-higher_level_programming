bffa9ea927f2ca5a108fb52e0bceebb11b2107d1
Otavie <otavieokuoyo@gmail.com>
2024-06-09 13:39:09 -0700

    Add GROUP BY to the script for 103-max_state.sql

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,4 +1,4 @@
 #!/usr/bin/env mysql
 -- Script to display the max temperature of each state ordered by state name
 
-SELECT state, MAX(value) FROM temperatures ORDER BY state DESC;
+SELECT state, MAX(value) as max_temp FROM temperatures GROUP BY state ORDER BY state DESC;
