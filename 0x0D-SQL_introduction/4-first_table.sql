f97488d60267419c93fbf75b47a58b76db93de13
Otavie <otavieokuoyo@gmail.com>
2024-06-09 09:54:59 -0700

    Add the TABLE and the IF stmts

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
 #!/usr/bin/env mysql
 -- Create a table called first_table in the MySQL server
 
-CREATE first_table (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
