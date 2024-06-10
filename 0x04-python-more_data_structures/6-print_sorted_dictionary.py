21702a9800402d1287fa7ee1210186ba5b64bea1
Otavie <otavieokuoyo@gmail.com>
2024-06-10 10:21:49 -0700

    Modified the code for 7-update_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
+    if a_dictionary is None:
+        return
     sorted_keys = sorted(a_dictionary)
     for a_key in sorted_keys:
         print(f"{a_key}: {a_dictionary[a_key]}")
