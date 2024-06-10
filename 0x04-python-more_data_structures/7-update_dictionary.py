c1ae839c39d71698568e32ba2255f32b5e873ae0
Otavie <otavieokuoyo@gmail.com>
2024-06-10 10:31:35 -0700

    Add some conditions

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    if key not in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        for each_key in a_dictionary:
+            if each_key == key:
+                a_dictionary[each_key] = value
+    return a_dictionary
