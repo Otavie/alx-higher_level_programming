51b478f6d25f1578e9a37b599f7ace23ec7ad2c7
Otavie <otavieokuoyo@gmail.com>
2024-06-10 10:46:15 -0700

    Removed the else statement

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
-    if key not in a_dictionary:
-        return
-    else:
+    if key in a_dictionary:
         del a_dictionary[key]
