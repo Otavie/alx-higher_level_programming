3b56c4f2487e8716acc5ae57760cc5c10f710d78
Otavie <otavieokuoyo@gmail.com>
2024-06-10 05:23:53 -0700

    Add the code for 4-only_diff_elements.py

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    union = []
+    for i in set_1:
+        for j in set_2:
+            if i != j:
+                unionn.append(i)
+    return (union)
