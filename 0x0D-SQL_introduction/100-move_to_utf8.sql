af5eab7d81c477d493f877b824457560a1729594
Otavie <otavieokuoyo@gmail.com>
2024-06-09 19:43:27 -0700

    Add db name for lines 7 and 9 for 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -4,7 +4,7 @@
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Convert the table
-ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Convert the column
-ALTER TABLE first_table MODIFY `name` VARCHAR(256) DEFAULT NULL;
+ALTER TABLE hbtn_0c_0.first_table MODIFY `name` VARCHAR(256) DEFAULT NULL;
