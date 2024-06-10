c939b87eb118f59d37b128f6bf300ac4248ab4ab
Otavie <otavieokuoyo@gmail.com>
2024-06-09 19:32:55 -0700

    Replaced max_temp with avg_temp

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,4 +1,4 @@
 #!/usr/bin/env mysql
 -- Script to display the top 3 cities
 
-SELECT city, AVG(value) AS max_temp FROM temperatures WHERE month IN (7, 8) GROUP BY city ORDER BY max_temp DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month IN (7, 8) GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
