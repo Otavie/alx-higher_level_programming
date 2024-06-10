511925f5eeb4b855ad08faca9808a9cd8725d377
Otavie <otavieokuoyo@gmail.com>
2024-06-10 05:39:00 -0700

    Complete overhaul of the code

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -3,8 +3,9 @@
 def only_diff_elements(set_1, set_2):
     union = []
     for i in set_1:
-        union.append(i)
-        for j in set_2:
-            if j not in union:
-                union.append(j)
+        if i not in set_2:
+            union.append(i)
+    for j in set_2:
+        if j not in set_1:
+            union.append(j)
     return (union)
