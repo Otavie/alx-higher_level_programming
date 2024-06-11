c499597a068db5adfed67e147da66b3a1afb0065
Otavie <otavieokuoyo@gmail.com>
2024-06-11 07:09:42 -0700

    Changed divison to division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -6,7 +6,7 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
-            print("divison by 0")
+            print("division by 0")
             result = 0
         except (TypeError, ValueError):
             print("wrong type")
