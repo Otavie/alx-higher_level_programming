91c46a901b5cd348193c7f701935f37c045db5c5
Otavie <otavieokuoyo@gmail.com>
2024-06-11 10:41:20 -0700

    Modified the code and import sys

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
+import sys
+
 def safe_print_integer_err(value):
     try:
-        values_as_int = int(value)
-        print("{:d}".format(value_as_int))
+        print("{:d}".format(value))
         return True
-    except ValueError:
-        print("Exception: ValueError occurred.", file=sys.stderr)
+    except (ValueError, TypeError) as e:
+        print("Exception: {}".format(e), file=sys.stderr)
         return False
