5af6c0616c7fb0e3f00e4051056d4c373b5f9b82
Otavie <otavieokuoyo@gmail.com>
2024-06-11 06:18:10 -0700

    Add result to TypeError and ValueError blocks

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -10,6 +10,7 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
         except (TypeError, ValueError):
             print("wrong type")
+            result = 0
         except IndexError:
             print("out of range")
             result = 0
