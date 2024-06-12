784d2833c5b49a680345495266132f5da2710394
Otavie <otavieokuoyo@gmail.com>
2024-06-11 19:40:03 -0700

    Add the code for 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Module that defines a class Square with a private instance attribute size
+"""
+
+
+class Square:
+    """
+    A clas Square wth a private instance attribute size.
+
+    Attributes:
+        __size (int): The size of the square (private) with default value of 0
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes the Square instance with a given size.
+
+        Args:
+            size (int): The size of the square (default is 0)
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Returns the area of the square
+        """
+        return size * size
