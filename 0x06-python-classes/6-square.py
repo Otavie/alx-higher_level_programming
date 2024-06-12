c3205a0bef1820aa278e41ad4afb31c0b88b9d0b
Otavie <otavieokuoyo@gmail.com>
2024-06-12 01:35:54 -0700

    Replaced all with all in line 89

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -86,7 +86,7 @@ class Square:
             TypeError: If value is not a tuple of 2 +ve integers
         """
         if not isinstance(value, tuple) or len(value) != 2 or \
-                not al(isinstance(num, int) and num>= 0 for num in value):
+                not all(isinstance(num, int) and num>= 0 for num in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
