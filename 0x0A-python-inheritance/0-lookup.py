2ef8c39965180e7c0fc078c3246de07bb3cfccc9
Otavie <otavieokuoyo@gmail.com>
2024-06-12 17:33:18 -0700

    used the dir keyword

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def lookup(obj):
-    """Returns the list of available attributes and methods of an obj"""
-    return obj.__doc__()
+    """Returns the list of available attributes and methods of an object"""
+
+    return dir(obj)
