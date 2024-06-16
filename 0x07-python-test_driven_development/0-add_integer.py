edaeec0d0673bfec9f1a4c4b660c40c914dd8849
Otavie <otavieokuoyo@gmail.com>
2024-06-15 21:07:05 -0700

    Add the script for 0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module to add two numbers"""
+
+def add_integer(a, b=98):
+    """Method to add two numbers"""
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
