799c2aff48646501e761c2b2342d8ae9847e51a5
Otavie <otavieokuoyo@gmail.com>
2024-06-15 23:46:51 -0700

    Do separate type check for float and NaN and convert float to int

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,4 +11,14 @@ def add_integer(a, b=98):
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    return int(a) + int(b)
+    if isinstance(a, float):
+        raise TypeError("a must be an integer")
+        if a != a:
+            a = int(a)
+
+    if isinstance(b, float):
+        raise TypeError("b must be an integer")
+        if b != b:
+            b = int(b)
+
+    return a + b
