35d8c732f687cf53547c1d13ebd850c3236d602b
otavie <otavieokuoyo@gmail.com>
2024-06-19 17:27:12 +0100

    Used === for comparison as opposed to ==

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@
 // Fet the arguments passed to the script
 const cmdArgs = process.argv.slice(2);
 
-if (cmdArgs == 0) {
+if (cmdArgs === 0) {
 	console.log('No argument');
-} else if (cmdArgs = 1) {
+} else if (cmdArgs === 1) {
 	console.log('Argument found');
 } else {
 	console.log('Arguments found');
