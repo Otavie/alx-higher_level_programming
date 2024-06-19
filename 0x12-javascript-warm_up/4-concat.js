38252be78bbc94e1d842202b09da0a8ee8818467
Otavie <otavieokuoyo@gmail.com>
2024-06-19 14:08:01 +0100

    Removed the 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-
-// Get the arguments passed to the script
-const args = process.argv.slice(2);
-
-console.log(args[0] + ' is ' + args[1]);
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-let i = 0;
-const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-while (i < 3) {
-  console.log(arr[i]);
-  i++;
-}
