3f59d75dee0137577a14b82d2a795be4754231f6
Otavie <otavieokuoyo@gmail.com>
2024-06-19 19:55:20 +0100

    Follow semi standard format

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,9 +4,9 @@
 const cmdArgs = process.argv.slice(2);
 
 if (cmdArgs.length === 0) {
-	console.log('No argument');
+    console.log('No argument');
 } else if (cmdArgs.length === 1) {
-	console.log('Argument found');
+    console.log('Argument found');
 } else {
-	console.log('Arguments found');
+    console.log('Arguments found');
 }
