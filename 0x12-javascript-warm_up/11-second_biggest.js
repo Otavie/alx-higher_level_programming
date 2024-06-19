4967bb78c9e07d311f02618d0128895b07d6bab1
Otavie <otavieokuoyo@gmail.com>
2024-06-19 21:11:19 +0100

    Add the code for 11-second_biggest.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// Fetch the arguments passed to the script
+const cmdArgs = process.argv.slice(2);
+const arrNumbers = cmdArgs.sort();
+
+if (arrNumbers.length <= 1) {
+  console.log(0);
+} else {
+  console.log(arrNumbers[arrNumbers.length - 2]);
+}
