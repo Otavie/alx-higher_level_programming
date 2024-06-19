70276d0c31c9b42a2e6e65817dd6ca670b474995
root <root@Otas>
2024-06-19 18:04:41 +0100

    Add .length ppty

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,10 +3,10 @@
 // Fet the arguments passed to the script
 const cmdArgs = process.argv.slice(2);
 
-if (cmdArgs === 0) {
+if (cmdArgs.length === 0) {
 	console.log('No argument');
-} else if (cmdArgs === 1) {
+} else if (cmdArgs.length === 1) {
 	console.log('Argument found');
-} else {
+} else if (cmdArgs.length > 1) {
 	console.log('Arguments found');
 }
