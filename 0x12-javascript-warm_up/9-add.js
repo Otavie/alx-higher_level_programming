7a8fb433eb645256bd7aa62a8e6b752726a07c8f
Otavie <otavieokuoyo@gmail.com>
2024-06-19 20:46:43 +0100

    Add the code for 9-add.js

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// Fetch the arguments passed to the script
+const cmdArgs = process.argv.slice(2);
+const arg1 = parseInt(cmdArgs[0]);
+const arg2 = parseInt(cmdArgs[1]);
+
+function add(a, b) {
+  return (a + b);
+}
+
+console.log(add(arg1, arg2));
