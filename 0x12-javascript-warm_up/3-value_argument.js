8f6c048d10bb3a3fd0270b3b20de24424dfe1fb7
Otavie <otavieokuoyo@gmail.com>
2024-06-19 20:18:53 +0100

    Modified the complete code

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,11 +2,8 @@
 
 // Fetch the arguments passed to the script
 const cmdArgs = process.argv.slice(2);
-let count = 0;
 
-for (const cmdArg of cmdArgs) { count++; }
-
-if (count === 0) {
+if (cmdArgs[0] === undefined) {
   console.log('No argument');
 } else {
   console.log(cmdArgs[0]);
