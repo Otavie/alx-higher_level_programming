26b5aabb6fb51c82fed5fb79d4db19be7d0c32a3
Otavie <otavieokuoyo@gmail.com>
2024-06-19 18:09:30 +0100

    Removed the last else if statement

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-// Fet the arguments passed to the script
+// Fetch the arguments passed to the script
 const cmdArgs = process.argv.slice(2);
 
 if (cmdArgs.length === 0) {
 	console.log('No argument');
 } else if (cmdArgs.length === 1) {
 	console.log('Argument found');
-} else if (cmdArgs.length > 1) {
+} else {
 	console.log('Arguments found');
 }
