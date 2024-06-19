3e105d8a140518b6e16a45988f13ead33c79ed9f
Otavie <otavieokuoyo@gmail.com>
2024-06-19 19:58:43 +0100

    Add template for 3-value_argument.js

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// Fetch the arguments passed to the script
+const cmdArgs = process.argv.slice(2);
+
+if (cmdArgs.length === 0) {
+  console.log('No argument');
+} else if (cmdArgs.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
