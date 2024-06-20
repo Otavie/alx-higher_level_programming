33bb206f9dab233fd18e7f16c56877e932c780dd
Otavie <otavieokuoyo@gmail.com>
2024-06-20 03:19:33 +0100

    Also checked if the arg != a number

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@
 const cmdArgs = process.argv.slice(2);
 let i = 1;
 let j = 1;
-if (cmdArgs[0] === undefined) {
+if (cmdArgs[0] === undefined || isNaN(cmdArgs)) {
   console.log('Missing size');
 } else {
   for (i = 0; i < cmdArgs[0]; i++) {
