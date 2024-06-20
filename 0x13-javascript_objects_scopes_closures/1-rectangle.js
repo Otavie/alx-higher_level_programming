ec87a394935c08514467c72d0456a32f2884b055
Otavie <otavieokuoyo@gmail.com>
2024-06-20 14:25:28 -0700

    Add the code for 1-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+class Rectangle {
+  // This is a class that defines a rectangle with width and height
+  constructor (width, height) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
