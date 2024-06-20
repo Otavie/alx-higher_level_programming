bf29ea413572791813b557119dd60b9ace7d88a7
Otavie <otavieokuoyo@gmail.com>
2024-06-20 14:37:58 -0700

    Add the code for 2-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+class Rectangle {
+  // This is a class that defines a rectangle with width and height
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      this.width = 0;
+      this.height = 0;
+    }
+  }
+}
+
+module.exports = Rectangle;
