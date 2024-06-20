1dddca7a32895631e428412ae0988524ad250ce1
Otavie <otavieokuoyo@gmail.com>
2024-06-20 03:18:13 +0100

    Add the code for 8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const cmdArgs = process.argv.slice(2);
+let i = 1;
+let j = 1;
+if (cmdArgs[0] === undefined) {
+  console.log('Missing size');
+} else {
+  for (i = 0; i < cmdArgs[0]; i++) {
+    let row = '';
+    for (j = 0; j < cmdArgs[0]; j++) {
+      row+= 'X';
+    }
+  console.log(row);
+  }
+}
