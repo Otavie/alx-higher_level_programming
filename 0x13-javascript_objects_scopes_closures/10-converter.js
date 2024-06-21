e82b7bc5d7c6968f6dac4455e98397001c1f39ce
Otavie <otavieokuoyo@gmail.com>
2024-06-20 21:07:56 -0700

    Add the code for 10-converter.js

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+exports.converter = function (base) {
+  function baseConverter (n) {
+    if (n < base) {
+      return String(n);
+    } else {
+      return baseConverter(Math.floor(n / base)) + String(n % base);
+    }
+  }
+
+  return function (num) {
+    return baseConverter(num);
+  }
+}
