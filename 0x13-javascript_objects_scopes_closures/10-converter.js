781e9b6922e60a85eaf4aed850a3395d78eec087
Otavie <otavieokuoyo@gmail.com>
2024-06-20 21:22:00 -0700

    Removed condtion that is not working

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -2,11 +2,7 @@
 exports.converter = function (base) {
   function baseConverter (n) {
     if (n < base) {
-      if (base === 16 && n >= 10 && n <= 15) {
-        return String.fromCharCode(n + 87);
-      } else {
-        return String(n);
-      }
+      return String(n);
     } else {
       return baseConverter(Math.floor(n / base)) + String(n % base);
     }
