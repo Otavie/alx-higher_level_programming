143243aca69e7c88f31ebcda1e086027a9005385
Otavie <otavieokuoyo@gmail.com>
2024-06-20 19:11:17 -0700

    Removed the reverse() method

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return list.reverse();
+  let newList = [];
+  let i = list.length - 1;
+  for (i; i >= 0; i--) {
+    newList.push(list[i]);
+  }
+  return newList;
 }
