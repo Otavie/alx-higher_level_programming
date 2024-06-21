5293ab457bf713c09a74323e5df59eb2ae89dba8
Otavie <otavieokuoyo@gmail.com>
2024-06-20 19:56:12 -0700

    Add the rotate method

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate (){
+    this.width = h;
+    this.height = w;
+  }
+}
+
+module.exports = Rectangle;
