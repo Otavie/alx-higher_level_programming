e2dbf7599d147cc17ab51ccefb858873a7b90dfb
Otavie <otavieokuoyo@gmail.com>
2024-09-02 05:41:48 -0700

    Removed double underscore in front of var names in __init__ method

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,8 +8,8 @@ This module contains a single class, Rectangle.
 class Rectangle:
     """Defines a rectangle with default width and height"""
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
