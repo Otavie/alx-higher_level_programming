2d402372a78aa3b178e2497c3d38974f93389f36
Otavie <otavieokuoyo@gmail.com>
2024-09-02 06:48:21 -0700

    Add the conditio for checking if any para is 0

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -40,7 +40,12 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """Returns the area of the rectangle"""
         return self.__width * self.__height
 
     def perimeter(self):
-        return 2 * (self.__width + self.__height)
+        """Returns the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
